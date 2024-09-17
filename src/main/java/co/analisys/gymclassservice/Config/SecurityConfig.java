d080d1c96b0850e94ebf91f7ebe3b7e4824b1b72
DaniBonica001 <danielabc_0106@hotmail.com>
2024-09-16 21:08:52 -0500

    Security enabled

--- src/main/java/co/analisys/gymclassservice/Config/SecurityConfig.java
+++ src/main/java/co/analisys/gymclassservice/Config/SecurityConfig.java
@@ -17,7 +17,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/circulacion/public/**").permitAll()
                         .requestMatchers("/**").permitAll()
